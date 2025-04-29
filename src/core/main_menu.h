994d68ff45a68d68a95cd95ce892c30bfe166bba
ben7147 <95097279+ben7147@users.noreply.github.com>
2025-04-29 15:44:26 +0200

    Update main_menu.h

--- src/core/main_menu.h
+++ src/core/main_menu.h
@@ -5,6 +5,7 @@
 
 #include "menu_items/BleMenu.h"
 #include "menu_items/ClockMenu.h"
+#include "menu_items/EXTLedControlMenu.h"
 #include "menu_items/ConfigMenu.h"
 #include "menu_items/ConnectMenu.h"
 #include "menu_items/FMMenu.h"
@@ -34,6 +35,7 @@ public:
     RFMenu rfMenu;
     ScriptsMenu scriptsMenu;
     WifiMenu wifiMenu;
+    EXTLedControlMenu ledMenu;
 
     MainMenu();
     ~MainMenu();
